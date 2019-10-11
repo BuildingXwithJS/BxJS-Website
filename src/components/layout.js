c2875e2f2cfc3f298f627748ff8fea4412c47ac0
Tim Ermilov <yamalight@gmail.com>
2019-10-11 17:54:07 +0000

    Finish episode list rendering

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@ function Layout({ children }) {
     <div className="flex flex-col font-sans min-h-screen text-gray-900">
       <Header />
 
-      <main className="flex flex-col flex-1 md:justify-center max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
+      <main className="flex flex-col flex-1 max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
         {children}
       </main>
     </div>
--- src/pages/episodesList.js
+++ src/pages/episodesList.js
@@ -5,9 +5,22 @@ import SEO from '../components/seo';
 
 function IndexPage({
   data: {
-    allLink: { distinct: episodes },
+    allLink: { edges },
   },
 }) {
+  const episodes = [];
+  edges
+    .map(({ node }) => ({
+      episodeUrl: node.data.episodeUrl,
+      episodeName: node.data.episodeName,
+    }))
+    .forEach(ep => {
+      if (episodes.find(e => e.episodeUrl === ep.episodeUrl)) {
+        return;
+      }
+      episodes.push(ep);
+    });
+
   return (
     <Layout>
       <SEO
@@ -15,19 +28,21 @@ function IndexPage({
         title="BxJS Weekly - All Episodes"
       />
 
-      <div>
-        {episodes
-          .sort(
-            (a, b) =>
-              Number(b.replace('Episode ', '')) -
-              Number(a.replace('Episode ', ''))
-          )
-          .map(episodeName => (
-            <div key={episodeName}>
-              <Link to={`/${episodeName}`}>{episodeName}</Link>
-            </div>
-          ))}
-      </div>
+      <h1 className="text-3xl	py-4">Episodes list</h1>
+
+      {episodes
+        .sort(
+          (a, b) =>
+            Number(b.episodeUrl.replace('/Episode', '')) -
+            Number(a.episodeUrl.replace('/Episode', ''))
+        )
+        .map(episode => (
+          <div key={episode.episodeUrl} className="py-1">
+            <Link className="text-lg" to={`/${episode.episodeUrl}`}>
+              {episode.episodeName}
+            </Link>
+          </div>
+        ))}
     </Layout>
   );
 }
@@ -37,7 +52,14 @@ export default IndexPage;
 export const pageQuery = graphql`
   query {
     allLink {
-      distinct(field: data___episodeName)
+      edges {
+        node {
+          data {
+            episodeUrl
+            episodeName
+          }
+        }
+      }
     }
   }
 `;
