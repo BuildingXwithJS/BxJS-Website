eddc71492d1a1967bdcf982d2688b5c1820e3d7b
Ron <laniado.ron@gmail.com>
2019-11-11 20:05:35 -0500

    Added text underline on hover

--- src/pages/episodesList.js
+++ src/pages/episodesList.js
@@ -18,7 +18,7 @@ function IndexPage({
       <h1 className="text-3xl	py-4">BxJS Weekly - Episodes list</h1>
 
       {episodes.map(episode => (
-        <div key={episode.node.id} className="py-1">
+        <div key={episode.node.id} className="py-1 hover:underline">
           <Link
             className="text-lg text-blue-700"
             to={`/${episode.node.data.episodeUrl}`}
