45b9b645a95e121e72c00f0bd1ea2c51e3f27e6e
8HoLoN <8holon@gmail.com>
2020-09-29 22:15:16 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,15 @@
 import 'loaders.css';
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './header';
+import { DarkTheme } from './theme.service';
 
 function Layout({ children }) {
+  const [isDarkThemeEnabled, setIsDarkThemeEnabled] = useState(DarkTheme.isEnabled);
+  DarkTheme.subscribe(setIsDarkThemeEnabled);
+
   return (
-    <div className="flex flex-col font-sans min-h-screen text-gray-600 bg-gray-900">
+    <div className={`${isDarkThemeEnabled?`text-gray-600 bg-gray-900`:`text-gray-900`} flex flex-col font-sans min-h-screen`} >
       <Header />
 
       <main className="flex flex-col flex-1 max-w-4xl mx-auto px-4 py-8 md:p-8 w-full">
