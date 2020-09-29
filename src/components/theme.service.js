0c9a418f5b198ed99f86b774d1044460ed5fd825
8HoLoN <8holon@gmail.com>
2020-09-29 22:14:12 +0200

    Create theme.service.js

---
+++ src/components/theme.service.js
@@ -0,0 +1,17 @@
+import React, { useRef, useState } from 'react';
+
+const DarkTheme = {
+    isEnabled: false,
+    subscribers: [],
+    subscribe(fn){
+        this.subscribers.push(fn);
+    },
+    next(v){
+        this.subscribers.forEach(fn=>fn(v));
+    }
+};
+DarkTheme.subscribe(v=>DarkTheme.isEnabled=v);
+
+export {
+    DarkTheme
+};
