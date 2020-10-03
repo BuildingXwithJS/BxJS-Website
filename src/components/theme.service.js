de45cdcd6af6f83dd19fdd178002d3fb92590d70
8HoLoN <8holon@gmail.com>
2020-10-03 15:41:36 +0200

    yarn lint fix

--- src/components/theme.service.js
+++ src/components/theme.service.js
@@ -1,19 +1,17 @@
 const localStorageKey = 'DarkThemeIsEnabled';
 const DarkTheme = {
-    isEnabled: JSON.parse(localStorage.getItem(localStorageKey)) || false,
-    subscribers: [],
-    subscribe(fn) {
-        this.subscribers.push(fn);
-    },
-    next(v) {
-        this.subscribers.forEach(fn => fn(v));
-    }
+  isEnabled: JSON.parse(localStorage.getItem(localStorageKey)) || false,
+  subscribers: [],
+  subscribe(fn) {
+    this.subscribers.push(fn);
+  },
+  next(v) {
+    this.subscribers.forEach(fn => fn(v));
+  },
 };
 DarkTheme.subscribe(isEnabled => {
-    DarkTheme.isEnabled = isEnabled;
-    localStorage.setItem(localStorageKey, JSON.stringify(DarkTheme.isEnabled));
+  DarkTheme.isEnabled = isEnabled;
+  localStorage.setItem(localStorageKey, JSON.stringify(DarkTheme.isEnabled));
 });
 
-export {
-    DarkTheme
-};
+export { DarkTheme };
