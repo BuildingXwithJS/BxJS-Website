146346aa39976351ece044a92f08412c6a8c8a5f
8HoLoN <8holon@gmail.com>
2020-10-03 15:44:05 +0200

    yarn lint fix

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,9 @@ import { DarkTheme } from './theme.service';
 
 function Header() {
   const [isExpanded, toggleExpansion] = useState(false);
-  const [isDarkThemeEnabled, setIsDarkThemeEnabled] = useState(DarkTheme.isEnabled);
+  const [isDarkThemeEnabled, setIsDarkThemeEnabled] = useState(
+    DarkTheme.isEnabled
+  );
   DarkTheme.subscribe(setIsDarkThemeEnabled);
 
   const { site } = useStaticQuery(graphql`
@@ -19,28 +21,37 @@ function Header() {
   `);
 
   return (
-    <header className={`${isDarkThemeEnabled?`bg-yellow-900`:`bg-yellow-600`}`}>
-                
+    <header
+      className={`${isDarkThemeEnabled ? `bg-yellow-900` : `bg-yellow-600`}`}
+    >
       <div className="flex flex-wrap items-center justify-between max-w-4xl mx-auto p-4">
         <div className="flex items-center gap-6">
-          <Link className={`${isDarkThemeEnabled?`text-gray-600`:`text-white`} flex items-center no-underline`} to="/">
+          <Link
+            className={`${
+              isDarkThemeEnabled ? `text-gray-600` : `text-white`
+            } flex items-center no-underline`}
+            to="/"
+          >
             <span className="font-bold text-xl tracking-tight">
               {site.siteMetadata.title}
             </span>
           </Link>
-          <button className={`${
-            isDarkThemeEnabled ? `bg-gray-900 hover:bg-gray-900` : `bg-white-900 hover:bg-white-900`
-            } text-gray-600 font-bold py-2 px-4 border border-blue-700 rounded`} 
-            onClick={() => DarkTheme.next(!DarkTheme.isEnabled)} 
+          <button
+            className={`${
+              isDarkThemeEnabled
+                ? `bg-gray-900 hover:bg-gray-900`
+                : `bg-white-900 hover:bg-white-900`
+            } text-gray-600 font-bold py-2 px-4 border border-blue-700 rounded`}
+            onClick={() => DarkTheme.next(!DarkTheme.isEnabled)}
           >
             Dark Mode
           </button>
         </div>
 
-        
-
         <button
-          className={`${isDarkThemeEnabled?`text-gray-600`:`text-white`} block md:hidden border border-white flex items-center px-3 py-2 rounded`}
+          className={`${
+            isDarkThemeEnabled ? `text-gray-600` : `text-white`
+          } block md:hidden border border-white flex items-center px-3 py-2 rounded`}
           onClick={() => toggleExpansion(!isExpanded)}
         >
           <svg
@@ -66,7 +77,9 @@ function Header() {
             },
           ].map(link => (
             <Link
-              className={`${isDarkThemeEnabled?`text-gray-600`:`text-white`} block md:inline-block mt-4 md:mt-0 md:ml-6 hover:underline hover:text-gray-300`}
+              className={`${
+                isDarkThemeEnabled ? `text-gray-600` : `text-white`
+              } block md:inline-block mt-4 md:mt-0 md:ml-6 hover:underline hover:text-gray-300`}
               key={link.title}
               to={link.route}
             >
