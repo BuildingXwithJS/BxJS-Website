a26e22145fd1c8221f3c5c1d9428a86f8267fe54
8HoLoN <8holon@gmail.com>
2020-10-03 15:36:40 +0200

    add local persistance

--- src/components/theme.service.js
+++ src/components/theme.service.js
@@ -1,14 +1,18 @@
+const localStorageKey = 'DarkThemeIsEnabled';
 const DarkTheme = {
-    isEnabled: false,
+    isEnabled: JSON.parse(localStorage.getItem(localStorageKey)) || false,
     subscribers: [],
-    subscribe(fn){
+    subscribe(fn) {
         this.subscribers.push(fn);
     },
-    next(v){
-        this.subscribers.forEach(fn=>fn(v));
+    next(v) {
+        this.subscribers.forEach(fn => fn(v));
     }
 };
-DarkTheme.subscribe(v=>DarkTheme.isEnabled=v);
+DarkTheme.subscribe(isEnabled => {
+    DarkTheme.isEnabled = isEnabled;
+    localStorage.setItem(localStorageKey, JSON.stringify(DarkTheme.isEnabled));
+});
 
 export {
     DarkTheme
