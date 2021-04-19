9c5680b4ea24202e97741b693443db6a6d978b75
Tim Ermilov <yamalight@gmail.com>
2021-04-19 15:16:02 +0200

    Use simpletransformers for twitter model training

---
+++ ml/README.md
@@ -0,0 +1,11 @@
+# Machine learning part of BxJS podcast
+
+## Notes
+
+Code currently uses CPU version of PyTorch, you might need to install it manually, like so:
+
+```bash
+$ pip3 install torch==1.8.1+cpu torchvision==0.9.1+cpu torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+```
+
+See [official website](https://pytorch.org/) for more info.
--- ml/train.py
+++ ml/train.py
@@ -1,104 +1,61 @@
 import json
-import os
-import shutil
+import logging
 
-import tensorflow as tf
-import tensorflow_hub as hub
-import tensorflow_text as text
-from official.nlp import optimization  # to create AdamW optmizer
+import pandas as pd
+from simpletransformers.classification import (ClassificationArgs,
+                                               ClassificationModel)
+
+logging.basicConfig(level=logging.INFO)
+transformers_logger = logging.getLogger("transformers")
+transformers_logger.setLevel(logging.WARNING)
 
 with open('../datasets/twitter/tweets-positive.json') as f:
     positiveExamples = [json.loads(line)['full_text'].lower() for line in f]
-    positiveExamplesLabels = [1 for item in positiveExamples]
 
 with open('../datasets/twitter/tweets-negative.json') as f:
     negativeExamples = [json.loads(line)['full_text'].lower() for line in f]
-    negativeExamplesLabels = [0 for line in negativeExamples]
 
 with open('../datasets/twitter/tweets-test-positive.json') as f:
     positiveTestSamples = [json.loads(line)['full_text'].lower() for line in f]
-    positiveTestSamplesLabels = [1 for line in positiveTestSamples]
 
 with open('../datasets/twitter/tweets-test-negative.json') as f:
     negativeTestSamples = [json.loads(line)['full_text'].lower() for line in f]
-    negativeTestSamplesLabels = [0 for line in negativeTestSamples]
 
-tfhub_handle_encoder = 'https://tfhub.dev/tensorflow/albert_en_base/2'
-tfhub_handle_preprocess = 'https://tfhub.dev/tensorflow/albert_en_preprocess/3'
+# Preparing train data
+train_data = [[item, 1] for item in positiveExamples] + [[item, 0]
+                                                         for item in negativeExamples]
+train_df = pd.DataFrame(train_data)
+train_df.columns = ["text", "labels"]
 
-print(f'BERT model selected           : {tfhub_handle_encoder}')
-print(f'Preprocess model auto-selected: {tfhub_handle_preprocess}')
+# Preparing eval data
+eval_data = [[item, 1] for item in positiveTestSamples] + [[item, 0]
+                                                           for item in negativeTestSamples]
+eval_df = pd.DataFrame(eval_data)
+eval_df.columns = ["text", "labels"]
 
-bert_preprocess_model = hub.KerasLayer(tfhub_handle_preprocess)
+# Optional model configuration
+model_args = ClassificationArgs(num_train_epochs=1, overwrite_output_dir=True)
 
-text_test = ['this is such an amazing movie!']
-text_preprocessed = bert_preprocess_model(text_test)
+# Create a ClassificationModel
+model = ClassificationModel(
+    "roberta", "roberta-base", args=model_args, use_cuda=False
+)
 
-print(f'Keys       : {list(text_preprocessed.keys())}')
-print(f'Shape      : {text_preprocessed["input_word_ids"].shape}')
-print(f'Word Ids   : {text_preprocessed["input_word_ids"][0, :12]}')
-print(f'Input Mask : {text_preprocessed["input_mask"][0, :12]}')
-print(f'Type Ids   : {text_preprocessed["input_type_ids"][0, :12]}')
+# Train the model
+model.train_model(train_df)
 
-bert_model = hub.KerasLayer(tfhub_handle_encoder)
+# Evaluate the model
+result, model_outputs, wrong_predictions = model.eval_model(eval_df)
 
-bert_results = bert_model(text_preprocessed)
+print('\n\n\n')
+print('result:', result)
+print('model_outputs:', model_outputs)
+print('wrong_predictions:', wrong_predictions)
+print('\n\n\n')
 
-print(f'Loaded BERT: {tfhub_handle_encoder}')
-print(f'Pooled Outputs Shape:{bert_results["pooled_output"].shape}')
-print(f'Pooled Outputs Values:{bert_results["pooled_output"][0, :12]}')
-print(f'Sequence Outputs Shape:{bert_results["sequence_output"].shape}')
-print(f'Sequence Outputs Values:{bert_results["sequence_output"][0, :12]}')
+# Make predictions with the model
+# predictions, raw_outputs = model.predict(["Sam was a Wizard"])
 
-
-def build_classifier_model():
-    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-    preprocessing_layer = hub.KerasLayer(
-        tfhub_handle_preprocess, name='preprocessing')
-    encoder_inputs = preprocessing_layer(text_input)
-    encoder = hub.KerasLayer(tfhub_handle_encoder,
-                             trainable=True, name='BERT_encoder')
-    outputs = encoder(encoder_inputs)
-    net = outputs['pooled_output']
-    net = tf.keras.layers.Dropout(0.1)(net)
-    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
-    return tf.keras.Model(text_input, net)
-
-
-classifier_model = build_classifier_model()
-bert_raw_result = classifier_model(tf.constant(text_test))
-print(tf.sigmoid(bert_raw_result))
-
-loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-metrics = tf.metrics.BinaryAccuracy()
-
-string_ds = tf.data.Dataset.from_tensor_slices([
-    positiveExamples + negativeExamples
-])
-label_ds = tf.data.Dataset.from_tensor_slices([
-    positiveExamplesLabels + negativeExamplesLabels
-])
-train_ds = tf.data.Dataset.zip((string_ds, label_ds))
-
-epochs = 5
-steps_per_epoch = tf.data.experimental.cardinality(train_ds).numpy()
-num_train_steps = steps_per_epoch * epochs
-num_warmup_steps = int(0.1*num_train_steps)
-
-init_lr = 3e-5
-optimizer = optimization.create_optimizer(init_lr=init_lr,
-                                          num_train_steps=num_train_steps,
-                                          num_warmup_steps=num_warmup_steps,
-                                          optimizer_type='adamw')
-
-classifier_model.compile(optimizer=optimizer,
-                         loss=loss,
-                         metrics=metrics)
-
-print(f'Training model with {tfhub_handle_encoder}')
-history = classifier_model.fit(x=train_ds, epochs=epochs)
-
-# loss, accuracy = classifier_model.evaluate(test_ds)
-
-# print(f'Loss: {loss}')
-# print(f'Accuracy: {accuracy}')
+# print('\n\n\n')
+# print('predictions:', predictions)
+# print('raw_outputs:', raw_outputs)
